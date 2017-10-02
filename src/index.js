17112af96e50cc1cac4ab6efdf65e27e31f23080
Matt Novinger <mnovinger@gmail.com>
2017-10-02 15:43:43 -0600

    allow for custom bindings

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import ngReduxProvider from './components/ngRedux';
 
+export const withCustomBinding = (angularBinding) => {
+  return angularBinding.module('ngRedux', [])
+  .provider('$ngRedux', ngReduxProvider)
+  .name;  
+}
+
 export default angular.module('ngRedux', [])
   .provider('$ngRedux', ngReduxProvider)
   .name;
